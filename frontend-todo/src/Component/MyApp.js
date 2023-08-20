e71fe8e7f6c5091119b94cc39193e73c07bbee83
Naman2002 <naman1442002@gmail.com>
2023-08-20 17:00:26 +0530

    update myapp

--- frontend-todo/src/Component/MyApp.js
+++ frontend-todo/src/Component/MyApp.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import Cus_Navbar from './CusNavbar'
+import CusNavbar from './CusNavbar'
 import { Outlet } from 'react-router-dom'
 
 function MyApp() {
     return (
         <> 
-            <Cus_Navbar />
+            <CusNavbar />
             <Outlet />
         
 
